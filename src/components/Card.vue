01d85aa7fc41d1636226eecabf3f48284e317de2
Ronildo Sousa <sousaronildo400@gmail.com>
2023-03-02 14:56:10 -0300

    update card and projects data

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,26 +10,24 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="w-[400px] max-w-sm p-2 mx-auto lg:max-w-md">
-    <div
-      class="overflow-hidden bg-gray-900 rounded-lg cursor-pointer bg-gradient-to-b from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700"
-    >
-      <div class="h-[220px]">
-        <img
-          class="object-fill w-full h-full rounded"
-          :src="image ?? 'https://picsum.photos/seed/picsum/400/300'"
-          alt="card-item"
-        />
-      </div>
-      <div class="flex flex-col items-center p-6">
-        <h1 class="mb-3 text-xl font-medium">{{ title }}</h1>
-        <p class="mb-3 break-words">
-          {{ description }}
-        </p>
-        <MyButton :uri="link">
-          {{ link_text }}
-        </MyButton>
-      </div>
+  <div
+    class="w-[400px] h-[500px] flex flex-col max-w-sm mx-auto lg:max-w-md overflow-hidden bg-gray-900 rounded-lg cursor-pointer bg-gradient-to-b from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700"
+  >
+    <div class="flex-grow-0 h-[200px] basis-auto">
+      <img
+        class="object-fill w-full h-full rounded"
+        :src="image ?? 'https://picsum.photos/seed/picsum/400/300'"
+        alt="card-item"
+      />
+    </div>
+    <div class="flex flex-col items-center justify-center flex-grow p-6 basis-auto">
+      <h1 class="mb-3 text-xl font-medium">{{ title }}</h1>
+      <p class="mb-3 text-center break-words">
+        {{ description }}
+      </p>
+      <MyButton :uri="link">
+        {{ link_text }}
+      </MyButton>
     </div>
   </div>
 </template>
--- src/components/List.vue
+++ src/components/List.vue
@@ -9,16 +9,16 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="relative rounded group">
-    <h1 :id="id" class="mt-5 mb-5 ml-3 text-3xl font-medium text-gray-200">{{ title ?? "Title" }}</h1>
+  <div class="rounded group">
+    <h1 :id="id" class="mb-5 ml-3 text-3xl font-medium text-gray-200">{{ title ?? "Title" }}</h1>
     <div class="overflow-x-scroll cursor-pointer scrollbar-thin scrollbar-thumb-blue-700 hover:scrollbar-thumb-blue-500 scrollbar-thumb-rounded-full">
       <div
-        class="transition duration-500 ease-in-out"
+        class="flex"
         :style="{width: projects.length * 400 + 'px' }"
         v-if="projects"
       >
         <Card
-          class="inline-block mb-5"
+          class="mx-3 mb-5"
           v-for="(project, idx) in projects"
           :key="idx"
           :image="project.image"
